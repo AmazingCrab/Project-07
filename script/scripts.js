1d2221e1738ccf69962ad2c3e1e1af457c9abbfa
Fenush <econtegarcia@gmail.com>
2023-06-21 17:33:40 -0300

    Constructors Added

---
+++ script/scripts.js
@@ -0,0 +1,14 @@
+function Book (title, author, pages, read){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+}
+
+Book.prototype.info = function (title, author,pages,read){
+    if (read === true)
+        {let readYet= "it was read."}
+        else
+        { let readYet= "was not read."}
+    console.log("The"+ this.title + "by" + this.author + "has" + this.pages + "pages" + readYet)
+}
