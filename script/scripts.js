ff6f530365bcb87a02b5413e6c49d29feffb21f5
Fenush <econtegarcia@gmail.com>
2023-06-21 22:35:30 -0300

    FormUiMaker() Added Book Structure by DOM

--- script/scripts.js
+++ script/scripts.js
@@ -1,3 +1,4 @@
+/*f() constructors*/
 function Book (title, author, pages, read){
     this.title=title;
     this.author=author;
@@ -12,4 +13,36 @@ Book.prototype.info = function (title, author,pages,read){
         { let readYet= "was not read."}
     console.log("The"+ this.title + "by" + this.author + "has" + this.pages + "pages" + readYet)
 }
+/*function add book UI*/
+const mainContainer = document.getElementById("main")
+document.getElementById("addButton").addEventListener("click", formUiMaker)
 
+function formUiMaker () {
+    const formContainer = document.createElement("div");
+    const titleLabel = document.createElement("label");
+    const titleInput = document.createElement("input");
+    const authorLabel = document.createElement("label");
+    const authorInput = document.createElement("input");
+    const pagesLabel = document.createElement("label");
+    const pagesInput = document.createElement("input");
+    const readLabel = document.createElement("label");
+    const readInput = document.createElement("input");
+    titleLabel.textContent = "Title:";
+    authorLabel.textContent = "Author:";
+    pagesLabel.textContent = "Pages:";
+    readLabel.textContent = "Read:";
+    titleInput.setAttribute("placeholder", "The title is...");
+    authorInput.setAttribute("placeholder", "Who wrote the book?");
+    pagesInput.setAttribute("placeholder", "How many ?")
+    mainContainer.appendChild(formContainer);
+    formContainer.appendChild(titleLabel);
+    formContainer.appendChild(titleInput)
+    formContainer.appendChild(authorLabel);
+    formContainer.appendChild(authorInput);
+    formContainer.appendChild(pagesLabel);
+    formContainer.appendChild(pagesInput);
+    formContainer.appendChild(readLabel);
+    formContainer.appendChild(readInput);
+    formContainer.classList.add("formStyle")
+    readInput.setAttribute("type", "checkbox")
+}
