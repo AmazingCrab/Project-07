1c08b539e81bc8db2efe656b9add958657f5c4df
Fenush <econtegarcia@gmail.com>
2023-06-26 19:00:38 -0300

    Now constructor Have a New Parameteter

--- script/scripts.js
+++ script/scripts.js
@@ -1,41 +1,60 @@
+let title, author, pages, read, readYet;
+const books = [];
 /*f() constructors*/
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.info();
+    }
+        info() {
+            if (read === true) 
+                {this.readYet = "YES";}
+            else 
+                {this.readYet = "NO";}
+        }
 }
 
-Book.prototype.info = function (title, author, pages, read) {
-    if (read === true) { let readYet = "it was read." }
-    else { let readYet = "was not read." }
-    console.log("The" + this.title + "by" + this.author + "has" + this.pages + "pages" + readYet)
-}
+const titleInput = document.createElement("input");
+const authorInput = document.createElement("input");
+const pagesInput = document.createElement("input");
+const readInput = document.createElement("input");    
+const formContainer = document.createElement("form");
+const formContainerBackGround = document.createElement("div");
+const titleLabel = document.createElement("label");
+const authorLabel = document.createElement("label");
+const pagesLabel = document.createElement("label");
+const readLabel = document.createElement("label");
+const submitButtonContainer = document.createElement("div")
+const submitButton = document.createElement("button");
+const addButtonContainer = document.getElementById("addButtonContainer")
+
 /*function add book UI*/
 const mainContainer = document.getElementById("main")
-document.getElementById("addButton").addEventListener("click", formUiMaker)
+document.getElementById("addButton").addEventListener("click", formUi)
 
-function formUiMaker() {
-    const formContainer = document.createElement("form");
-    const formContainerBackGround = document.createElement("div");
-    const titleLabel = document.createElement("label");
-    const titleInput = document.createElement("input");
-    const authorLabel = document.createElement("label");
-    const authorInput = document.createElement("input");
-    const pagesLabel = document.createElement("label");
-    const pagesInput = document.createElement("input");
-    const readLabel = document.createElement("label");
-    const readInput = document.createElement("input");
-    const submitButtonContainer = document.createElement("div")
-    const submitButton = document.createElement("button");
-    const addButtonContainer = document.getElementById("addButtonContainer")
+function formUi() {
     titleLabel.textContent = "Title:";
     authorLabel.textContent = "Author:";
     pagesLabel.textContent = "Pages:";
     readLabel.textContent = "Read:";
+    submitButton.textContent = "Submit";
+
     titleInput.setAttribute("placeholder", "The title is...");
     authorInput.setAttribute("placeholder", "Who wrote the book?");
     pagesInput.setAttribute("placeholder", "How many ?")
+    titleInput.setAttribute ("name" , "title");
+    authorInput.setAttribute("name" , "author");
+    pagesInput.setAttribute ("name" , "pages");
+    readInput.setAttribute  ("name" , "read");
+    titleInput.setAttribute("type", "Text");
+    authorInput.setAttribute("type", "text");
+    pagesInput.setAttribute("type", "number");
+    readInput.setAttribute("type", "checkbox");
+    submitButton.setAttribute("value", "submit")
+    submitButton.setAttribute("type", "submit")
+
     mainContainer.appendChild(formContainer);
     formContainer.appendChild(formContainerBackGround)
     formContainerBackGround.appendChild(titleLabel);
@@ -48,21 +67,21 @@ function formUiMaker() {
     formContainerBackGround.appendChild(readInput);
     formContainer.appendChild(submitButtonContainer);
     submitButtonContainer.appendChild(submitButton);
+
     formContainerBackGround.classList.add("formStyle");
-    readInput.setAttribute("type", "checkbox");
     submitButtonContainer.classList.add("submitButtonContainer");
     submitButton.classList.add("submitButton");
-    submitButton.setAttribute("type", "submit")
-    submitButton.textContent = "Submit";
     main.removeChild(addButtonContainer)
+
+    formContainer.addEventListener("submit", submit);
+    /*function submit*/
+    function submit (e){
+        e.preventDefault();{
+        main.appendChild(addButtonContainer);
+        mainContainer.removeChild(formContainer);
+        read= readInput.checked;
+        const addBook = new book(titleInput.value , authorInput.value, pagesInput.value,read );
+        books.push(addBook);
+    }
 }
-
-
-/*function submit*/
-submitButton.addEventListener("click", submitRemoveUi);
-function submitRemoveUi (e){
-    e.preventDefault();
-    mainContainer.removeChild(formContainer)
 }
-
-
